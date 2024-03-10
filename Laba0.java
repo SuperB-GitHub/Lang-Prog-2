e5ff7ff5f4680c19f40120ff9ea6749829fa97e4
SuperB-GitHub <148558050+SuperB-GitHub@users.noreply.github.com>
2024-03-10 16:39:21 +0300

    Добавление

---
+++ Laba0.java
@@ -0,0 +1,236 @@
+package Labs;
+
+public class Laba0 {
+    public static int a=1;
+    public static int b=3;
+    public static int c=9;
+    public static int d=27;
+    public static void main(String[] args) {
+        main11(args);}
+
+    public static void main0(String[] args) {
+        System.out.println("Рурурy");}
+
+    public static void main1(String[] args) {
+        System.out.print("Hello world\n"+"Not hello world");}
+
+    public static void main2(String[] args) {
+        for(int i=1; i<=5; i++){
+            System.out.println("Hello world "+ i);}}
+
+    public static void main3(String[] args) {
+        int a=4;
+        int b=3;
+        int c=a-b;
+        System.out.println(Integer.toString(c));}
+
+    public static void main4(String[] args) {
+        int x = 2;
+        int y = 12;
+        x=y-x;
+        y=y+x;
+        System.out.println(x);
+        System.out.println(y);}
+
+    public static void main5(String[] args) {
+        System.out.print("Курва ");
+        // System.out.print("Бобер ");
+        System.out.print("Ужик ");
+        // System.out.print("Ежик ");
+        System.out.print("Хомик ");
+    }
+    public static void main6(String[] args) {
+        String priv="Привет";
+        System.out.printf("%s",priv);}
+
+    public static void main7(String[] args) {
+        int num=2;
+        Integer.toString(num);
+        System.out.println(num*num);}
+
+    public static void main8(String[] args) {
+        for(int i=1;i<11;i++){
+            System.out.println("yea");
+        }
+    }
+
+    public static void main9(String[] args) {
+        String s="Оценка ";
+        System.out.println(s);
+        // System.out.print("5");
+        System.out.print("4");
+        // System.out.print("3");
+        // System.out.println("Удовл.");
+        System.out.println("Хорошо");
+        // System.out.println("Отлично");
+    }
+
+    public static void main10(String[] args) {
+        String dg="DS";
+        String text=dg+"TU"+"!";
+        System.out.println(text);
+    }
+
+    public static void main11(String[] args) {
+        int res=-a+b-c+d;
+        System.out.println(res);
+    }
+    public static void main12(String[] args) {
+        System.out.println(sqr(5));
+    }
+    public static int sqr(int a){
+        return a * a;
+    }
+    public static void main13(String[] args) {
+        // int a = 1;
+        double b = 1.5;
+        double c = b + 1.5;
+        // int d = a + 12;
+        // double e = 12.3;
+        // String s = "Luke, " + a;
+        String s1 = "Twice ";
+        // String s2 = "a";
+        String s3 = s1 + "the pride, ";
+        String s4 = " the fall.";
+        System.out.println(s3 + c + s4);
+    }
+    public static void main14(String[] args) {
+        print("The power is easy to use!");
+        print("The power opens many opportunities!");}
+    public static void print(String s) {
+        for (int i=1;i<3;i++){
+            System.out.println(s);}   
+    }
+    public static void main15(String[] args) {
+        count(5);
+    }
+    public static void count(int n) {
+        System.out.println("Первая степень числа "+n+" = "+n);
+        System.out.println("Квадрат числа "+n+" = "+n*n);
+        System.out.println("Куб числа "+n+" = "+n*n*n);
+        System.out.println("Четвертая степень числа "+n+" = "+n*n*n*n);
+    }
+    public static void main16(String[] args) {
+        say("Mike", "black");
+    }
+    static void say(String name, String color){
+        System.out.println("Hello, my name is "+name+" and my cat is "+color);
+    }
+
+
+    public static void main17(String[] args) {
+        Owner owner_1 = new Owner();
+        Pet pet_1 = new Pet();
+        owner_1.name = "Mike";
+        pet_1.kind = "cat";
+        pet_1.color = "black";
+        owner_1.pet = pet_1;
+        owner_1.say();
+    }
+    static class Owner{
+        String name;
+        Pet pet;
+        void say(){
+            System.out.println("Hello, my name is " + name + " and my " + pet.kind + " is " + pet.color);
+        }
+    }
+    static class Pet{
+         String kind;
+         String color;
+    }
+
+
+    public static void main18(String[] args) {
+        increaseSpeed(700);
+    }
+    static void increaseSpeed(int s){
+        System.out.println("Your speed is: "+(s+100)+" km/h.");
+    }
+
+    public static void main19(String[] args) {
+        Zam zam = new Zam();
+        Dron dron = new Dron();
+        zam.spy = dron;
+        dron.hunter=zam;
+    } 
+    public static class Zam {
+        public int kam;
+        public int dam;
+        public Dron spy;
+    }
+    public static class Dron {
+        public int kron;
+        public int pron;
+        public Zam hunter;
+    }
+    public static void main20(String[] args) {
+        Jedi jedi1 = new Jedi();
+        jedi1.name = "Obi-Wan";
+        Jedi jedi2 = new Jedi();
+        jedi2.name = "Anakin";
+        Jedi jedi3 = new Jedi();
+        jedi3.name = "Joda";
+    }
+    public static class Jedi {
+        public String name;
+    }
+    public static void main21(String[] args) {
+        // Clone clone1 = new Clone();
+        // Clone clone2 = new Clone();
+        // Clone clone3 = new Clone();
+        // Clone clone4 = new Clone();
+        // Clone clone5 = new Clone();
+        // Clone clone6;
+        // Clone clone7;
+    }
+    public static class Clone {}
+
+    public static void main22(String[] args) {
+        Clone1 clone1 = new Clone1();
+        Clone2 clone2 = new Clone2();
+        Clone3 clone3 = new Clone3();
+        Dias dias = new Dias();
+        clone1.owner=dias;
+        clone2.owner=dias;
+        clone3.owner=dias;
+    }
+    public static class Clone1 {
+        public Dias owner;
+    }
+    public static class Clone2 {
+        public Dias owner;
+    }
+    public static class Clone3 {
+        public Dias owner;
+    }
+    public static class Dias {
+    }
+    public static void main23(String[] args) {
+        System.out.println(getWeight(888));
+    }
+    public static double getWeight(int weight) {
+        weight=weight/6;
+        return weight;
+    }
+
+    public static void main24(String[] args) {
+        print3("dump");
+        print3("cargo");
+    }
+    public static void print3(String s) {
+        System.out.println(s + " " + s + " " + s + " ");
+    }
+
+    public static void main25(String[] args) {
+        System.out.println(min(12, 33));
+        System.out.println(min(-20, 0));
+        System.out.println(min(-10, -20));
+    }
+    public static int min (int a, int b) {
+        if (a<b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
---
+++ Laba1.java
@@ -0,0 +1,146 @@
+package Labs;
+
+import java.util.Arrays;
+
+public class Laba1 {
+    public static void main(String[] args) {
+        main8(args);
+    }
+    public static void main1(String[] args) {
+        int[] X = {20,3,4,12,16,31,50,10,45,36,12,87,65,43,2,3,6,45,4,90};
+        int[] Y = {12,23,34,5,67,43,6,7,30,49,50,42,21,56,70};
+        double z;
+        z=((Math.pow(Math.E, Math.abs(getMax(Y)))-Math.pow(Math.E, Math.abs(getMax(X))))/(Math.sqrt(Math.abs(getMax(X)*getMax(Y)))));
+        System.out.println(z);
+    }
+    public static int getMax(int[] mass) {
+        Arrays.sort(mass);
+        return mass[mass.length-1];
+    }
+    public static int getMin(int[] mass) {
+        Arrays.sort(mass);
+        return mass[0];
+    }
+    public static void main2(String[] args) {
+        int[] S = {50,10,45,36,12,65};
+        int[] T = {20,3,4,12,16,31};
+        int[] K = {43,2,3,6,4,90};
+        double M = (getSum(S)+getSum(T)+getSum(K))/2;
+        System.out.println(M);
+    }
+    public static int getSum(int[] mass) {
+        int fin=0;
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i]>0) {
+                fin = fin+mass[i];}}
+        return fin;
+    }
+    public static void main3(String[] args) {
+        int c;
+        int m=7;
+        int n=5;
+        c=getFact(m)/(getFact(n)*(getFact(m-n)));
+        System.out.println(c);
+        
+    }
+    public static int getFact(int m) {
+        int fin=1;
+        for (int i = 1; i <= m; i++) {
+            fin=fin*i;}
+        return fin;
+    }
+    public static void main4(String[] args) {
+        int x=3;
+        int y=4;
+        int z=5;
+        double s=getSinSqrt(x, y)+getSinSqrt(y, z)+getSinSqrt(x, z);
+        System.out.println(s);
+    }
+    public static double getSinSqrt(int a, int b) {
+        return Math.sqrt(Math.pow(a, 2)+Math.pow(b,2)+Math.pow(Math.sin(a*b),2));
+    }
+    public static void main5(String[] args) {
+        int[] X = {20,3,4,12,16,31,50,10,45,36,12,87,65,43,2,3,6,45,4,90};
+        int[] Y = {12,23,34,5,67,43,6,7,30,49,50,42,21,56,70};
+        int[] Z = {4,7,12,6,45,32,-8,10,11,25};
+        System.out.println("Значение X: "+getAvg(X));
+        System.out.println("Значение Y: "+getAvg(Y));
+        System.out.println("Значение Z: "+getAvg(Z));
+    }
+    public static double getAvg (int[] mass) {
+        double fin=0;
+        int i2=0;
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i]>0) {
+                fin=fin+mass[i];
+                ++i2;}}
+        return fin/i2;
+    }
+    public static void main6(String[] args) {
+        int[] A = {20,3,4,12,-16,31,-50,10,45,36,12,87,65,-43,2,3,6,45,4,90};
+        int[] B = {12,23,34,5,67,43,6,7,30,49,50,42,21,56,70};
+        int[] C = {4,7,12,6,45,32,-8,10,11,25,-3,0};
+        System.out.println(getAnswer(A, B, C));
+        
+    }
+    public static double getAnswer(int[] mass_a,int[] mass_b,int[] mass_c) {
+        if (Math.abs(getMin(mass_a))>10) {
+            return getMin(mass_b)+getMin(mass_c);
+        } else {
+            return 1+Math.abs(getMin(mass_c));
+        }
+    }
+    public static void main7(String[] args) {
+        int[] A = {20,3,4,12,-16,31,-50,10,45,36,12,87,65,-43,2,3,6,
+            45,4,90,12,23,34,5,67,43,6,7,30,49,50,42,21,56,70};
+        System.out.println(getAvgGeo(A));
+    }
+    public static double getAvgGeo(int[] mass) {
+        double fin=1;
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i]<12 && mass[i]>0)  {
+                fin=fin*mass[i];}}
+        return Math.sqrt(fin);
+    }
+    public static void main8(String[] args) {
+        int[] A = {20,3,4,12,-16,31,-50,10,45,36,12,87,65,-43,2,3,6,
+            45,4,90,12,23,34,5,-67,43,6,7,30,49,50,42,21,56,70,-7,12,6,
+            45,32,-8,10,11,25,-3,0};
+        System.out.println(getOtrNot(A));
+    }
+    public static double getOtrNot(int[] mass) {
+        int fin=0;
+        int count=0;
+        for (int i : mass) {
+            if (i<0 && i%2!=0) {
+                fin=fin+i;
+                count+=1;}}
+        System.out.println(fin);
+        return count;
+    }
+    public static void main9(String[] args) {
+        int[] A = {20,3,4,12,-16,31,-50,10,45,36,12,87,65,-43,2,3,6,
+            45,4,90,12,23,34,5,-67,43,6,7,30,49,50,42,21,56,70,-7,12,6,
+            45,32,-8,10,11,25,-3,0};
+        System.out.println(getPlAvg(A));
+    }
+    public static int getPlAvg(int[] mass) {
+        int fin=0;
+        for (int i : mass) {
+            fin=fin+i;}
+        return fin;
+    }
+    public static void main10(String[] args) {
+        int[] A = {20,3,4,12,-16,31,-50,10,45,36,12,87,65,-43,2,3,6,
+            45,4,90,12,23,34,5,-67,43,6,7,30,49,50,42,21,56,70,-7,12,6,
+            45,32,-8,10,11,25,-3,0};
+        System.out.println(A);
+    }
+    // public static int[] getSort(int[] mass){
+    //     int[] mass2;
+    //     int in=23;
+    //     for (int i = 0; i < mass.length; i++) {
+    //         // int u = in[0];
+    //     }
+    // }
+}
