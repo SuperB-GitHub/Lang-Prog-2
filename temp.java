2b57ed94ae35510400b23f2c41a7a8f0a4833b2f
SuperB-GitHub <mr.covalets2015@yandex.ru>
2024-04-22 11:56:35 +0300

    Просто

--- temp.java
+++ temp.java
@@ -2,12 +2,12 @@ import java.util.Arrays;
 
 public class temp {
     public static void main(String[] args) {
-        String[] value = {"AB","BC","AC"};
-        String a = Arrays.toString(value);
-        System.out.println(a);
-        System.out.println(a.contains("AB"));
-        System.out.println(a.contains("BC"));
-        System.out.println(a.contains("CD"));
-        System.out.println(a.contains("AE"));
-    }
-}
+        String text = "Вы в курсе, что я вас понял?  вас понял, вы в курсе?";
+        String[] words = text.split(" ");
+        
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j] + "s") || words[i].equals(words[j] + "ed") || words[i].equals(words[j] + "ing")) {
+                    System.out.println("Пара слов: " + words[i] + " и " + words[j]);
+                }
+            }}}}
